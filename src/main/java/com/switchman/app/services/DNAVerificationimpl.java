72ad84d953e9cc9a50427c6e471fda1160eec198
94MMartinez <mm.martinezmaximiliano@gmail.com>
2022-05-13 14:41:19 -0300

    changes names of variables

--- src/main/java/com/switchman/app/services/DNAVerificationimpl.java
+++ src/main/java/com/switchman/app/services/DNAVerificationimpl.java
@@ -1,144 +1,145 @@
 package com.switchman.app.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.switchman.app.repositories.DNARepository;
 
 @Service
 public class DNAVerificationimpl implements DNAVerificationService {
-    
-	@Autowired
-	public DNARepository dnaRepo;
+
+	
+	public boolean isMutant(String[] dna) {
+
+		return mutantPerLine(dna) + mutantPerColumn(dna) + mutantPerDiagonal(dna) + mutantPerContraDiagonal(dna) >= 2;
+	}
+
 	
 	
 	
-	 public boolean isMutant(String[] dna) {
+	private int mutantPerContraDiagonal(String[] dna) {
+		
+		if (dna.length == 0) {
+			return 0;
+		}
+		
+		int someDiagonalMutant = 0;
+		for (int actualRow = 0; actualRow < dna.length && ((dna.length - actualRow) >= 4); actualRow++) {
+			int letterEquals = 0;
+			int rowToCompare = actualRow + 1;
+			int actualColumn = dna.length - 1;
+			int columnToCompare = actualColumn - 1;
+			for (int j = dna.length - 1; (actualColumn >= 4 - 1); j--) {
+				if (dna[actualRow].charAt(actualColumn) == dna[rowToCompare].charAt(columnToCompare)) {
+					rowToCompare += 1;
+					columnToCompare -= 1;
+					letterEquals = letterEquals + 1;
+					if (letterEquals == 3) {
+						someDiagonalMutant = someDiagonalMutant + 1;
+						letterEquals = 0;
+						actualColumn -= 1;
+						columnToCompare = actualColumn - 1;
+						rowToCompare = actualRow + 1;
+					}
+				} else {
+					letterEquals = 0;
+					actualColumn -= 1;
+					columnToCompare = actualColumn - 1;
+					rowToCompare = actualRow + 1;
 
-    return mutantPerLine(dna) + mutantPerColumn(dna) + mutantPerDiagonal(dna) + mutantPerContraDiagonal(dna) >= 2;
-}
+				}
+			}
+		}
+		return someDiagonalMutant;
+	}
 
-    private int mutantPerContraDiagonal(String[] dna) {
-        if (dna.length == 0) {
-            return 0;
-        }
-        int someDiagonalMutant = 0;
-        for (int i = 0; i < dna.length && ((dna.length - i) >= 4); i++) {
-            int counter = 0;
-            int aux = i+1;
-            int aux2 = dna.length - 1;
-            int aux3 = aux2 - 1;
-            for (int j = dna.length - 1; ( aux2 >= 4 - 1) ; j--) {         
-                if (dna[i].charAt(aux2) == dna[aux].charAt(aux3)) {
-                    aux += 1;
-                    aux3 -= 1;
-                    counter = counter + 1;
-                    if (counter == 3) {
-                        someDiagonalMutant = someDiagonalMutant + 1;
-                        counter = 0;
-                        aux2 -= 1;
-                        aux3 = aux2 - 1;
-                        aux = i + 1;
-                    }
-                } else {
-                    counter = 0;
-                    aux2 -= 1;
-                    aux3 = aux2 - 1;
-                    aux = i + 1;
+	
+	
+	private int mutantPerDiagonal(String[] dna) {
+		if (dna.length == 0) {
+			return 0;
+		}
+		int someDiagonalMutant = 0;
+		for (int actualRow = 0; actualRow < dna.length && ((dna.length - actualRow) >= 4); actualRow++) {
+			int letterEquals = 0;
+			int rowToCompare = actualRow + 1;
+			int actualColumn = 0;
+			int columnToCompare = 1;
+			for (int j = 0; ((dna.length - actualColumn) >= 4) && rowToCompare < dna.length; j++) {
+				if (dna[actualRow].charAt(actualColumn) == dna[rowToCompare].charAt(columnToCompare)) {
+					rowToCompare += 1;
+					columnToCompare += 1;
+					letterEquals = letterEquals + 1;
+					if (letterEquals == 3) {
+						someDiagonalMutant = someDiagonalMutant + 1;
+						letterEquals = 0;
+					}
+				} else {
+					letterEquals = 0;
+					actualColumn += 1;
+					columnToCompare = actualColumn + 1;
+					rowToCompare = actualRow + 1;
 
-                }
-            }
-        }
-        return someDiagonalMutant;
-    }
+				}
+			}
+		}
+		return someDiagonalMutant;
+	}
 
-    private int mutantPerDiagonal(String[] dna) {
-        if (dna.length == 0) {
-            return 0;
-        }
-        int someDiagonalMutant = 0;
-        for (int i = 0; i < dna.length && ((dna.length - i) >= 4) ; i++) {
-            int counter = 0;
-            int aux = i+1;
-            int aux2 = 0;
-            int aux3 = 1;
-            for (int j = 0; ((dna.length - aux2) >= 4) && aux < dna.length; j++) {        
-                if (dna[i].charAt(aux2) == dna[aux].charAt(aux3)) {
-                    aux += 1;
-                    aux3 +=1;
-                    counter = counter + 1;
-                    if(counter == 3){
-                        someDiagonalMutant =  someDiagonalMutant + 1;
-                        counter = 0;
-                    }
-                }else {
-                    counter = 0;
-                    aux2 += 1;
-                    aux3 = aux2 + 1;
-                    aux = i + 1;
+	
+	private int mutantPerLine(String[] dna) {
+		if (dna.length == 0) {
+			return 0;
+		}
+		int someLineMutant = 0;
+		for (int actualRow = 0; actualRow < dna.length; actualRow++) {
+			int letterEquals = 0;
+			int actualColumn = 0;
+			int columnToCompare = 1;
+			for (int j = 0; columnToCompare < dna[0].length() && letterEquals < 3; j++) {
+				if (dna[actualRow].charAt(actualColumn) != dna[actualRow].charAt(columnToCompare)) {
+					actualColumn = columnToCompare;
+					columnToCompare += 1;
+					letterEquals = 0;
+				} else {
+					columnToCompare += 1;
+					letterEquals = letterEquals + 1;
+					if (letterEquals == 3) {
+						someLineMutant = someLineMutant + 1;
+						letterEquals = 0;
+					}
+				}
+			}
+		}
+		return someLineMutant;
+	}
 
-                }
-            }
-        }
-        return someDiagonalMutant;
-    }
+	
+	private int mutantPerColumn(String[] dna) {
+		if (dna.length == 0) {
+			return 0;
+		}
+		int someColumMutant = 0;
 
-    private int mutantPerLine(String[] dna) {
-        if (dna.length == 0) {
-            return 0;
-        }
-        int someLineMutant = 0;
-        for(int i = 0; i < dna.length ; i ++){
-            int counter = 0;
-            int aux = 0;
-            int aux2 = 1;
-            for (int j = 0 ;aux2 < dna[0].length() && counter < 3; j++) {                
-                if (dna[i].charAt(aux) != dna[i].charAt(aux2)) {
-                    aux = aux2;
-                    aux2 +=1;
-                    counter = 0;
-                }
-                else{
-                    aux2 +=1;
-                    counter = counter + 1;
-                    if(counter == 3){
-                        someLineMutant = someLineMutant + 1;
-                        counter = 0;
-                    }
-                }
-            }
-        }
-        return someLineMutant;
-    }
-
-    private int  mutantPerColumn(String[] dna) {
-        if (dna.length == 0) {
-            return 0;
-        }
-        int someColumMutant = 0;
-
-        for (int i = 0; i < dna.length && ((dna.length - i) >= 4); i++) {
-            int aux2 = i+1;
-            int counter = 0;
-            int aux = 0;
-            for (int j = 0;aux < dna.length ; j++) {
-                if( dna[i].charAt(aux) != dna[aux2].charAt(aux)){
-                    counter = 0;
-                    aux +=1;
-                    aux2 = i + 1;
-                }else {
-                    aux2 += 1;
-                    counter += 1;
-                    if(counter == 3){
-                        counter = 0;
-                        someColumMutant = someColumMutant + 1;
-                        aux2 = i + 1;
-                        aux +=1;
-                    }
-                }
-            }
-        }
-        return someColumMutant;
-    }
+		for (int actualRow = 0; actualRow < dna.length && ((dna.length - actualRow) >= 4); actualRow++) {
+			int rowToCompare = actualRow + 1;
+			int letterEquals = 0;
+			int actualColumn = 0;
+			for (int j = 0; actualColumn < dna.length; j++) {
+				if (dna[actualRow].charAt(actualColumn) != dna[rowToCompare].charAt(actualColumn)) {
+					letterEquals = 0;
+					actualColumn += 1;
+					rowToCompare = actualRow + 1;
+				} else {
+					rowToCompare += 1;
+					letterEquals += 1;
+					if (letterEquals == 3) {
+						letterEquals = 0;
+						someColumMutant = someColumMutant + 1;
+						rowToCompare = actualRow + 1;
+						actualColumn += 1;
+					}
+				}
+			}
+		}
+		return someColumMutant;
+	}
 
 }
