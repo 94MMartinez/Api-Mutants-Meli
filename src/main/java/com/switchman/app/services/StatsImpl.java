0cc67e402b8f32c0fe5b22d93088609bb3704e77
94MMartinez <mm.martinezmaximiliano@gmail.com>
2022-05-20 12:08:09 -0300

    code refactor

--- src/main/java/com/switchman/app/services/StatsImpl.java
+++ src/main/java/com/switchman/app/services/StatsImpl.java
@@ -1,11 +1,15 @@
 package com.switchman.app.services;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.switchman.app.dto.DnaInformationDTO;
 import com.switchman.app.dto.StatsDTO;
 import com.switchman.app.entities.DNA;
 import com.switchman.app.repositories.DNARepository;
+import com.switchman.app.util.DNAToBd;
 
 @Service
 public class StatsImpl implements StatsService {
@@ -18,6 +22,13 @@ public class StatsImpl implements StatsService {
 		long countHuman = dnaRepository.countAllByIsMutant(false);
 		StatsDTO stats = new StatsDTO(countMutants, countHuman);
 		return stats;
+	}
+
+	public void saveDNA(DnaInformationDTO dna, boolean isMutant) {
+		DNA dnaIsinRepo = findBydnaInformation(Arrays.toString(dna.getDna()));
+		if (dnaIsinRepo == null) {
+			saveDNA(new DNAToBd().transformerDNADtoToDNA(dna, isMutant));
+		}
 	}
 
 	@Override
